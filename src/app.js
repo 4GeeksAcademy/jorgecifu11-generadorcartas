1dc1998c5586600ffd72b41543e8dfed24b55e99
Jorge Cifuentes Pérez <jorge.cifuentes.perez@gmail.com>
2025-06-01 10:36:16 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+let numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+let pintas = ['♥', '♦', '♣', '♠'];
+
+let generarCarta = () => {
+  let rdmNumber = numeros[Math.floor(Math.random() * numeros.length)];
+  let rdmPintas = pintas[Math.floor(Math.random() * pintas.length)]
+
+  let numeroElem = document.getElementById('numero');
+  let izqElem = document.getElementById('izq');
+  let derElem = document.getElementById('der');
+  let carta = document.getElementById('carta');
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  numeroElem.textContent = rdmNumber;
+  izqElem.textContent = rdmPintas;
+  derElem.textContent = rdmPintas;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  if (rdmPintas === '♥' || rdmPintas === '♦') {
+    izqElem.classList.remove('negro');
+    izqElem.classList.add('rojo');
+    derElem.classList.remove('negro');
+    derElem.classList.add('rojo');
+
+  } 
+}
+
+window.onload = generarCarta;
